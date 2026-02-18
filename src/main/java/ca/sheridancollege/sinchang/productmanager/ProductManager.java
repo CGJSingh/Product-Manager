9a88e14690aaedc03248ba1980b5aedd47055ad7
Chan Gaganjeet Singh <sinchang@sheridancollege.ca>
2026-02-18 13:57:44 -0500

    Initial Commit by Chan Gaganjeet Singh

---
+++ src/main/java/ca/sheridancollege/sinchang/productmanager/ProductManager.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ca.sheridancollege.sinchang.productmanager;
+
+/**
+ *
+ * @author singh
+ */
+public class ProductManager {
+
+    public static void main(String[] args) {
+        System.out.println("Product Manager Application Started");
+    }
+}
